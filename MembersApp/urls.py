76b6b2f9e49935d5ad35dc3d5ec52041159d4c53
vionaosiako <viona.osiako@student.morigaschool.com>
2022-06-22 17:03:20 +0300

    Create log out functionality

--- MembersApp/urls.py
+++ MembersApp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('registerPage', views.registerPage, name='registerPage'),
     path('loginPage', views.loginPage, name='loginPage'),
+    path('logoutUser', views.logoutUser, name='logoutUser'),
 ]
